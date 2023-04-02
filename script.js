9bd16cacf586b95f9f841113d3b7dfcebaaaaca1
Artyom <128896821+AAudzeyeu@users.noreply.github.com>
2023-04-02 17:20:53 +0300

    end

--- script.js
+++ script.js
@@ -1,107 +1,156 @@
 
 
-// // const elem = document.querySelectorAll('.biig');
+// // // const elem = document.querySelectorAll('.biig');
 
-// // document.documentElement.addEventListener('click', (e) => {
-// //     const biig = e.target.closest('.biig') 
-// //     const color = 'red';
+// // // document.documentElement.addEventListener('click', (e) => {
+// // //     const biig = e.target.closest('.biig') 
+// // //     const color = 'red';
 
-// //     biig.style.background = color;
-// //     let scrollButtom = elem.scrollHeight - elem.scrollTop - elem.clientHeight;
+// // //     biig.style.background = color;
+// // //     let scrollButtom = elem.scrollHeight - elem.scrollTop - elem.clientHeight;
 
-// //     console.log(`TOP ${elem.scrollTop}`);
-// //     console.log(`BOTTOM ${scrollButtom}`);
-
-
-// // });
+// // //     console.log(`TOP ${elem.scrollTop}`);
+// // //     console.log(`BOTTOM ${scrollButtom}`);
 
 
-// let div = document.createElement('div');
-
-// div.style.overflowY = 'scroll';
-// div.style.width = '50px';
-// div.style.height = '50px';
-
-// // мы должны вставить элемент в документ, иначе размеры будут равны 0
-// document.body.append(div);
-// let scrollWidth = div.offsetWidth - div.clientWidth;
-
-// div.remove();
-
-// console.log(scrollWidth);
+// // // });
 
 
-// const ball = document.querySelector('#ball')
-// const field = document.querySelector('#field')
-// console.log(`Поле полная ширина mar + bord + pad + val - ${field.offsetWidth}`);
-// console.log(`Поле ширина клиента padd + val - ${field.clientWidth}`);
-// console.log(`Мяч полная ширина - ${ball.offsetWidth}`);
-// console.log(`Мяч ширина клиента - ${ball.clientWidth}`);
-// console.log(ball.clientWidth / 2);
-// const ballTop = field.clientHeight / 2 - ball.offsetHeight / 2;
-// const ballLeft = field.clientWidth / 2 - ball.offsetHeight / 2;
-// console.log();
-// ball.style.top = ballTop + 'px';
-// ball.style.left = ballLeft + 'px'
-// // ball.style.left = 'calc(50% - 20px)'
+// // let div = document.createElement('div');
+
+// // div.style.overflowY = 'scroll';
+// // div.style.width = '50px';
+// // div.style.height = '50px';
+
+// // // мы должны вставить элемент в документ, иначе размеры будут равны 0
+// // document.body.append(div);
+// // let scrollWidth = div.offsetWidth - div.clientWidth;
+
+// // div.remove();
+
+// // console.log(scrollWidth);
 
 
-// // let elem1 = document.getElementById("ball");
+// // const ball = document.querySelector('#ball')
+// // const field = document.querySelector('#field')
+// // console.log(`Поле полная ширина mar + bord + pad + val - ${field.offsetWidth}`);
+// // console.log(`Поле ширина клиента padd + val - ${field.clientWidth}`);
+// // console.log(`Мяч полная ширина - ${ball.offsetWidth}`);
+// // console.log(`Мяч ширина клиента - ${ball.clientWidth}`);
+// // console.log(ball.clientWidth / 2);
+// // const ballTop = field.clientHeight / 2 - ball.offsetHeight / 2;
+// // const ballLeft = field.clientWidth / 2 - ball.offsetHeight / 2;
+// // console.log();
+// // ball.style.top = ballTop + 'px';
+// // ball.style.left = ballLeft + 'px'
+// // // ball.style.left = 'calc(50% - 20px)'
 
-// // function createMessageUnder(elem1, html) {
-// //   // создаём элемент, который будет содержать сообщение
-// //   let message = document.createElement('div');
-// //   // для стилей лучше было бы использовать css-класс здесь
-// //   message.style.cssText = "position:absolute; color: red";
 
-// //   // устанавливаем координаты элементу, не забываем про "px"!
-// //   let coords = elem1.getBoundingClientRect();
+// // // let elem1 = document.getElementById("ball");
 
-// //   message.style.left = coords.left + "px";
-// //   message.style.top = coords.bottom + "px";
+// // // function createMessageUnder(elem1, html) {
+// // //   // создаём элемент, который будет содержать сообщение
+// // //   let message = document.createElement('div');
+// // //   // для стилей лучше было бы использовать css-класс здесь
+// // //   message.style.cssText = "position:absolute; color: red";
 
-// //   message.innerHTML = html;
+// // //   // устанавливаем координаты элементу, не забываем про "px"!
+// // //   let coords = elem1.getBoundingClientRect();
 
-// //   return message;
+// // //   message.style.left = coords.left + "px";
+// // //   message.style.top = coords.bottom + "px";
+
+// // //   message.innerHTML = html;
+
+// // //   return message;
+// // // }
+
+// // // // Использование:
+// // // // добавим сообщение на страницу на 5 секунд
+// // // let message = createMessageUnder(elem, 'Hello, world!');
+// // // document.body.append(message);
+// // // setTimeout(() => message.remove(), 5000);
+
+// // const fieldCord = field.getBoundingClientRect()
+// // let fieldCordOutHeight = field.offsetHeight - field.clientHeight;
+// // let fieldCordOurWidth = field.offsetWidth - field.clientWidth;
+
+// // let answer3 = [fieldCord.left + field.clientLeft, fieldCord.top + field.clientTop];
+// // let arr1 = [fieldCord.left, fieldCord.top];
+// // let arr2 = [fieldCord.right, fieldCord.bottom];
+// // let arr3 = [fieldCordOurWidth, fieldCordOutHeight];
+// // console.log(arr1);
+// // console.log(arr2);
+// // console.log(arr3);
+// // console.log(answer3);
+// // console.log(field.clientLeft);
+
+// // const button = document.querySelector('#button')
+
+// // const removeText = function () {
+// //     button.hidden = true;
 // // }
+// // button.addEventListener('click', removeText);
 
-// // // Использование:
-// // // добавим сообщение на страницу на 5 секунд
-// // let message = createMessageUnder(elem, 'Hello, world!');
-// // document.body.append(message);
-// // setTimeout(() => message.remove(), 5000);
 
-// const fieldCord = field.getBoundingClientRect()
-// let fieldCordOutHeight = field.offsetHeight - field.clientHeight;
-// let fieldCordOurWidth = field.offsetWidth - field.clientWidth;
+// const field = document.querySelector('#field');
+// const ball = document.querySelector('#ball')
 
-// let answer3 = [fieldCord.left + field.clientLeft, fieldCord.top + field.clientTop];
-// let arr1 = [fieldCord.left, fieldCord.top];
-// let arr2 = [fieldCord.right, fieldCord.bottom];
-// let arr3 = [fieldCordOurWidth, fieldCordOutHeight];
-// console.log(arr1);
-// console.log(arr2);
-// console.log(arr3);
-// console.log(answer3);
-// console.log(field.clientLeft);
+// let getPositionCursor = function (event) {
+//     let fieldCoords = this.getBoundingClientRect();
+//     let ballCoords = {
+//         top: event.clientY - fieldCoords.top - field.clientTop - ball.clientHeight / 2,
+//         left: event.clientX - fieldCoords.left - field.clientLeft - ball.clientWidth / 2
+//     };
 
-// const button = document.querySelector('#button')
+//     // запрещаем пересекать верхнюю границу поля
+//     if (ballCoords.top < 0) ballCoords.top = 0;
 
-// const removeText = function () {
-//     button.hidden = true;
+//     // запрещаем пересекать левую границу поля
+//     if (ballCoords.left < 0) ballCoords.left = 0;
+
+
+//     // // запрещаем пересекать правую границу поля
+//     if (ballCoords.left + ball.clientWidth > field.clientWidth) {
+//         ballCoords.left = field.clientWidth - ball.clientWidth;
+//     }
+
+//     // запрещаем пересекать нижнюю границу поля
+//     if (ballCoords.top + ball.clientHeight > field.clientHeight) {
+//         ballCoords.top = field.clientHeight - ball.clientHeight;
+//     }
+
+//     ball.style.left = ballCoords.left + 'px';
+//     ball.style.top = ballCoords.top + 'px';
+//     console.log(`ClientX ${event.clientX}`);
+//     console.log(`ClientY ${event.clientY}`);
+//     console.log(`clientWidth ${event.currentTarget.clientWidth}`);
+//     console.log(`clientHeight ${event.currentTarget.clientHeight}`);
+
+
 // }
-// button.addEventListener('click', removeText);
+// field.addEventListener('click', getPositionCursor)
 
 
-const field = document.querySelector('#field');
-const ball = document.querySelector('#ball')
+// const removeButton = document.querySelectorAll('.remove-button');
+// removeButton.addEventListener('click', removeText);
 
-let getPositionCursor = function(event) {
-    console.log(`ClientX ${event.clientX}`);
-    console.log(`ClientY ${event.clientY}`);
-    console.log(`clientWidth ${event.currentTarget.clientWidth}`);
-    console.log(`clientHeight ${event.currentTarget.clientHeight}`);
-    ball.style.left = event.clientX + 'px'
-    ball.style.top = event.clientY + 'px'
+// const removeText = function() {
+//     console.log(1);
+// }
+
+// container.onclick = function(event) {
+//     if (event.target.className != 'remove-button') return;
+
+//     let pane = event.target.closest('.pane');
+//     pane.remove();
+//   };
+
+const removeButton = document.querySelector('#container');
+const removeText = function(event) {
+    if(event.target.className != 'remove-button') return;
+
+    let pane = event.target.closest('.pane');
+    pane.remove();
 }
-field.addEventListener('click', getPositionCursor)
+removeButton.addEventListener('click', removeText)
