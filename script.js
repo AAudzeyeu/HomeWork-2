9c824f7e7703035d47b5243f84ef0b59abb7e578
Artyom <128896821+AAudzeyeu@users.noreply.github.com>
2023-04-14 18:59:09 +0300

    add Promise.all API

--- script.js
+++ script.js
@@ -6,35 +6,38 @@ const createForm = function () {
         'https://webznam.ru/_bl/16/41608074.jpg',
         'https://lh3.googleusercontent.com/2VWx9PZWWEXS9mQp__-M1pH_RO99sl-axc9NiGE02gHx0lOi_DJ7pcL9lt_ZtMlVNdL3lR8GogMfFGWAWDUdnAzwfzI=w640-h400-e365-rj-sc0x00ffffff'];
     const ul = document.createElement('ul');
-    
+
     document.body.prepend(ul);
     ul.classList.add('main-ul');
 
-    const createImgForm = async function () {
-        await imgUrl.forEach(function (element) {
-            const li = document.createElement('li');
-            const figure = document.createElement('figure');
-            const img = document.createElement('img');
-            ul.prepend(li);
+    const createImg = async function () {
 
-            img.classList.add('main-img')
-            img.style.width = 400 + 'px';
-            img.style.height = 250 + 'px';
-            img.src = element;
-            li.append(figure)
-            figure.append(img);
-        })
+        Promise.all(imgUrl)
+            .then(imgUrl.forEach(function (element) {
+                const li = document.createElement('li');
+                const figure = document.createElement('figure');
+                const img = document.createElement('img');
+                ul.prepend(li);
+
+                img.classList.add('main-img')
+                img.style.width = 400 + 'px';
+                img.style.height = 250 + 'px';
+                img.src = element;
+
+                li.append(figure)
+                figure.append(img);
+            }))
     }
-    createImgForm();
+    createImg()
 
     const mainImg = document.querySelector('.main-ul');
     const urlImg = document.createElement('a');
     const figcaption = document.createElement('figcaption');
-   
-    mainImg.addEventListener('click', function(e) {
- 
+
+    mainImg.addEventListener('click', function (e) {
+
         if (e.target.classList.value === 'main-img') {
-            
+
             urlImg.href = e.target.src
             urlImg.textContent = 'Ссылка на картинку'
             urlImg.target = '_blank'
