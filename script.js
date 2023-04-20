8cf5bc14c278ea398857853517ec83f1edce97ae
Artyom <128896821+AAudzeyeu@users.noreply.github.com>
2023-04-20 16:40:08 +0300

    last

--- script.js
+++ script.js
@@ -1,46 +1,123 @@
 const createTasksForm = function () {
     const buttonAddNewTask = document.querySelector('#button-add__task');
+    const buttonAddRandomTask = document.querySelector('#button-add__randomTask')
     const inputTask = document.querySelector('#add-task');
     const tasks = document.querySelector('.container-tasks')
+    const headerTasksMenu = document.querySelector('.tasks');
     const tasksCompleted = document.querySelector('.tasks-completed')
-    
+
     inputTask.setAttribute('required', true);
 
 
+    fetch('https://jsonplaceholder.typicode.com/todos/')
+        .then(response => response.json())
+        .then(json => {
+            let { completed, inProgress } = json.reduce((prev, cur) => {
+                if (cur.completed) {
+                    prev.completed.push(cur)
+                } else {
+                    prev.inProgress.push(cur)
+                } return prev;
+            },
+                {
+                    completed: [],
+                    inProgress: []
+                })
 
-    buttonAddNewTask.addEventListener('click', function (e) {
-        if (inputTask.value.length >= 1) {
-        e.preventDefault();
-        console.log();
-        // const br = document.createElement('br');
-        const inputAddNewTask = document.createElement('input');
-        const labelAddNewTask = document.createElement('label');
-        const headerTasksMenu = document.querySelector('.tasks');
+            buttonAddRandomTask.addEventListener('click', function (e) {
+                e.preventDefault();
+                completed.forEach(function(el, i) {
+                    
+                    const div = document.createElement('div');
+                    console.log();
+                    const inputAddNewTask = document.createElement('input');
+                    const labelAddNewTask = document.createElement('label');
+        
+                    inputAddNewTask.type = 'checkbox';
+                    inputAddNewTask.name = 'new-task'
+                    inputAddNewTask.classList.add('task-form__text');
 
-        inputAddNewTask.type = 'checkbox';
-        inputAddNewTask.name = 'new-task'
-        labelAddNewTask.setAttribute('for', inputAddNewTask.name);
-        labelAddNewTask.textContent = inputTask.value;
+                    labelAddNewTask.setAttribute('for', inputAddNewTask.name);
+                    labelAddNewTask.textContent = inProgress[i]['title'] + '&#10006;' ;
+                    labelAddNewTask.classList.add('task-form__text');
+        
+                    console.log();
+                    div.append(inputAddNewTask);
+                    inputAddNewTask.after(labelAddNewTask)
+                    headerTasksMenu.append(div)
+                })
+                
+                inProgress.forEach(function(el, i) {
+                    const div = document.createElement('div');
+                    console.log();
+                    const inputAddNewTask = document.createElement('input');
+                    const labelAddNewTask = document.createElement('label');
+        
+                    inputAddNewTask.type = 'checkbox';
+                    inputAddNewTask.name = 'new-task'
+                    inputAddNewTask.checked = 'checked';
+                    inputAddNewTask.classList.add('task-form__text');
 
-        console.log();
-            headerTasksMenu.append(inputAddNewTask);
-            inputAddNewTask.after(labelAddNewTask)
-            // labelAddNewTask.after(br);
+                    labelAddNewTask.setAttribute('for', inputAddNewTask.name);
+                    labelAddNewTask.textContent = inProgress[i]['title'];
+                    labelAddNewTask.classList.add('task-form__text');
+        
+                    console.log();
+                    div.append(inputAddNewTask);
+                    inputAddNewTask.after(labelAddNewTask)
+                    tasksCompleted.append(div)
+                })
+            })
 
-            console.log();
-        }
-    })
+            buttonAddNewTask.addEventListener('click', function (e) {
+        
+                if (inputTask.value.length >= 1) {
+                    e.preventDefault();
+                    const div = document.createElement('div')
+                    console.log();
+                    
+                    const inputAddNewTask = document.createElement('input');
+                    const labelAddNewTask = document.createElement('label');
+        
+                    inputAddNewTask.type = 'checkbox';
+                    inputAddNewTask.name = 'new-task';
+                    inputAddNewTask.id = 'new-task';
+                    inputAddNewTask.classList.add('task-form__text');
 
-    tasks.addEventListener('change', function(e) {
-        const headerTasksMenu = document.querySelector('.tasks');
-        // const br = document.createElement('br');
+                    labelAddNewTask.setAttribute('for', inputAddNewTask.name);
+                    labelAddNewTask.textContent = inputTask.value;
+                    labelAddNewTask.classList.add('task-form__text');
 
+                    completed.push(inputTask.value)
+                    console.log(completed);
+        
+                    console.log();
+                    div.append(inputAddNewTask);
+                    inputAddNewTask.after(labelAddNewTask)
+                    headerTasksMenu.append(div)
+        
+                    console.log();
+                }
+            })
+        })
+
+
+
+
+    tasks.addEventListener('change', function (e) {
+        
+        const inputNextSibling = e.target.nextSibling
+        const parent = inputNextSibling.parentNode;
+        console.log(parent);
         if (e.target.checked) {
-            tasksCompleted.append(e.target, e.target.nextSibling)
+            tasksCompleted.append(parent)
+            
         }
         if (!(e.target.checked)) {
-            headerTasksMenu.append(e.target, e.target.nextSibling)
+            headerTasksMenu.append(parent)
+            
         }
     })
 }
+
 createTasksForm();
