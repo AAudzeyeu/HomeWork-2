c9a18d78f35ab71dcbe217e1d61add376f45fb5f
1 <1>
2023-04-22 12:50:51 +0300

    last

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+// // const miniCssExtractPlugin = require('mini-css-extract-plugin')
+
+// // module.exports = {
+// //     entry: '/src/index.js',
+// //     output: {
+// //         filename: 'bundle.js',
+// //         path: '/dist'
+// //     },
+// //     resolve: {
+// //         extensions: ['.js'], 
+// //     },
+// //     module: {
+// //         rules: [
+// //             {
+// //                 test: /\.scss$/,
+// //                 use [
+// //                     'style-loader'
+// //                 ]
+// //             }
+// //         ]
+// //     }
+// // }
+
+// const miniCssExtractPlugin = require('mini-css-extract-plugin')
+
+// module.exports = {
+//     mode: 'development',
+//     entry: '/src/index.js',
+//     output: {
+//         filename: 'bundle.js',
+//         path: '/dist'
+//     },
+//     resolve: {
+//         extensions: ['.js'], 
+//     },
+// }
