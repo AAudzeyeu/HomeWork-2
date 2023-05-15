d09e3b3a04915fbe4fb416d93fbb913c29fe62e7
Artyom <128896821+AAudzeyeu@users.noreply.github.com>
2023-05-15 23:56:01 +0300

    last

--- src/index.js
+++ src/index.js
@@ -1,2 +1,27 @@
 
 // import './style.css'
+
+const addMovie = function() {
+    const button = document.querySelector('.button-add__movie');
+    const container = document.querySelector('.content-container');
+    const body = document.querySelector('body');
+    const addMovieContainer = document.querySelector('.add-movie__container');
+    
+    button.addEventListener('click', function() {
+        container.style.opacity = 0.5;
+        body.style.overflowY = 'hidden';
+        addMovieContainer.style.display = 'block';
+        
+    }) 
+    addMovieContainer.addEventListener('click', function(e) {
+        if (e.target.classList.contains('closeModal')) {
+            container.style.opacity = 1;
+            body.style.overflowY = 'auto';
+            addMovieContainer.style.display = 'none';
+        }
+    })
+}
+
+
+addMovie();
+
